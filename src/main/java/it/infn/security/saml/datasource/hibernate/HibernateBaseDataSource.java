b7d435a90e1f40fbdc7520a4b7d73baafd32fe1c
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2015-07-30 14:33:44 +0200

    Split data source

---
+++ src/main/java/it/infn/security/saml/datasource/hibernate/HibernateBaseDataSource.java
@@ -0,0 +1,226 @@
+package it.infn.security.saml.datasource.hibernate;
+
+import it.infn.security.saml.datasource.DataSource;
+import it.infn.security.saml.datasource.DataSourceException;
+import it.infn.security.saml.datasource.jpa.AttributeEntity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.opensaml.Configuration;
+import org.opensaml.saml2.core.Attribute;
+import org.opensaml.saml2.core.AttributeValue;
+import org.opensaml.saml2.core.impl.AttributeBuilder;
+import org.opensaml.xml.XMLObject;
+import org.opensaml.xml.XMLObjectBuilderFactory;
+import org.opensaml.xml.schema.XSString;
+import org.opensaml.xml.schema.impl.XSStringBuilder;
+
+public abstract class HibernateBaseDataSource
+    implements DataSource {
+
+    private static final Logger logger = Logger.getLogger(HibernateBaseDataSource.class.getName());
+
+    /*
+     * Definitions for specific attribute structures
+     */
+    protected final static String SPID_ATTR_NAME = "SPIDAttributes";
+
+    protected final static String KEY_FIELD = "key";
+
+    protected final static String CONTENT_FIELD = "content";
+
+    protected final static String ATTR_DESCR_FIELD = "description";
+
+    protected static SessionFactory sessionFactory;
+
+    public HibernateBaseDataSource() {
+    }
+
+    public void init()
+        throws DataSourceException {
+
+        try {
+
+            StandardServiceRegistryBuilder serviceRegistryBuilder = new StandardServiceRegistryBuilder();
+            serviceRegistryBuilder.applySettings(HibernateUtils.getHibernateConfig().getProperties());
+            sessionFactory = HibernateUtils.getHibernateConfig().buildSessionFactory(serviceRegistryBuilder.build());
+
+        } catch (Throwable th) {
+            logger.log(Level.SEVERE, "Cannot initialize database", th);
+            throw new DataSourceException("Cannot initialize database", th);
+        }
+
+    }
+
+    public List<Attribute> findAttributes(String id, List<Attribute> requiredAttrs)
+        throws DataSourceException {
+
+        ArrayList<Attribute> result = new ArrayList<Attribute>();
+
+        XMLObjectBuilderFactory builderFactory = Configuration.getBuilderFactory();
+        AttributeBuilder attributeBuilder = (AttributeBuilder) builderFactory
+                .getBuilder(Attribute.DEFAULT_ELEMENT_NAME);
+        XSStringBuilder attributeValueBuilder = (XSStringBuilder) builderFactory.getBuilder(XSString.TYPE_NAME);
+
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+
+            session.beginTransaction();
+
+            String qStr = "SELECT qUser.id FROM UserEntity as qUser WHERE qUser.userName = :uName";
+            Query query2 = session.createQuery(qStr).setString("uName", id);
+            Long userId = (Long) query2.uniqueResult();
+            if (userId == null) {
+                logger.info("Entity not found " + id);
+                /*
+                 * TODO empty list or error?
+                 */
+                // session.getTransaction().commit();
+                throw new DataSourceException("User not found");
+            }
+
+            HashSet<Long> allIds = getAllGroupIds(session, userId);
+            allIds.add(userId);
+
+            HashMap<String, Object> qArgs = new HashMap<String, Object>();
+            StringBuffer queryStr = new StringBuffer("SELECT qAttributes");
+            queryStr.append(" FROM ResourceEntity as qRes INNER JOIN qRes.attributes as qAttributes");
+
+            queryStr.append(" WHERE qRes.id IN (:resourceIds)");
+            qArgs.put("resourceIds", allIds);
+
+            if (requiredAttrs != null && requiredAttrs.size() > 0) {
+                queryStr.append(" AND (");
+                int keyNum = 0;
+                for (Attribute reqAttr : requiredAttrs) {
+                    String tmpName = reqAttr.getName();
+                    List<XMLObject> tmpValues = reqAttr.getAttributeValues();
+
+                    if (keyNum > 0) {
+                        queryStr.append(" OR");
+                    }
+                    String keyTag = "key_" + keyNum;
+                    keyNum++;
+
+                    if (tmpValues != null && tmpValues.size() > 0) {
+                        int cntNum = 0;
+                        for (XMLObject xObj : tmpValues) {
+
+                            if (cntNum > 0) {
+                                queryStr.append(" OR");
+                            }
+                            String refValue = xObj.getDOM().getTextContent().trim();
+
+                            queryStr.append(" (qAttributes.attributeId.key = :").append(keyTag);
+                            qArgs.put(keyTag, tmpName);
+
+                            String cntTag = "cnt_" + keyNum + "_" + cntNum;
+                            queryStr.append(" AND qAttributes.attributeId.content = :");
+                            queryStr.append(cntTag).append(")");
+                            qArgs.put(cntTag, refValue);
+                            cntNum++;
+                        }
+                    } else {
+                        queryStr.append(" qAttributes.attributeId.key = :").append(keyTag);
+                        qArgs.put(keyTag, tmpName);
+                    }
+                }
+
+                queryStr.append(")");
+            }
+
+            Query query = session.createQuery(queryStr.toString());
+            query.setProperties(qArgs);
+
+            @SuppressWarnings("unchecked")
+            List<AttributeEntity> filteredAttrs = query.list();
+
+            HashMap<String, Attribute> resultTable = new HashMap<String, Attribute>();
+            for (AttributeEntity attrEnt : filteredAttrs) {
+
+                String attrKey = attrEnt.getAttributeId().getKey();
+                Attribute attribute = null;
+                if (resultTable.containsKey(attrKey)) {
+                    attribute = resultTable.get(attrKey);
+                } else {
+                    attribute = attributeBuilder.buildObject();
+                    attribute.setName(attrKey);
+                    attribute.setNameFormat(Attribute.BASIC);
+                    resultTable.put(attrKey, attribute);
+                }
+
+                XSString attributeValue = attributeValueBuilder.buildObject(AttributeValue.DEFAULT_ELEMENT_NAME,
+                        XSString.TYPE_NAME);
+                attributeValue.setValue(attrEnt.getAttributeId().getContent());
+                attribute.getAttributeValues().add(attributeValue);
+
+            }
+
+            for (Attribute tmpAttr : resultTable.values()) {
+                result.add(tmpAttr);
+            }
+
+            session.getTransaction().commit();
+
+        } catch (Throwable th) {
+
+            session.getTransaction().rollback();
+
+            logger.log(Level.SEVERE, th.getMessage(), th);
+
+        }
+
+        return result;
+    }
+
+    public void close()
+        throws DataSourceException {
+
+    }
+
+    protected HashSet<Long> getDirectGroupIds(Session session, Long resId) {
+        StringBuffer queryStr = new StringBuffer("SELECT rGroups.id");
+        queryStr.append(" FROM ResourceEntity as resource INNER JOIN resource.groups as rGroups");
+        queryStr.append(" WHERE resource.id=?");
+        Query query = session.createQuery(queryStr.toString());
+        @SuppressWarnings("unchecked")
+        List<Long> idList = query.setLong(0, resId).list();
+        return new HashSet<Long>(idList);
+    }
+
+    protected HashSet<Long> getIndirectGroupIds(Session session, HashSet<Long> resIds) {
+        HashSet<Long> result = new HashSet<Long>();
+        HashSet<Long> currSet = resIds;
+        while (currSet.size() > 0) {
+            StringBuffer queryStr = new StringBuffer("SELECT rGroups.id");
+            queryStr.append(" FROM ResourceEntity as resource INNER JOIN resource.groups as rGroups");
+            queryStr.append(" WHERE resource.id IN (:resourceIds)");
+            Query query = session.createQuery(queryStr.toString());
+            @SuppressWarnings("unchecked")
+            List<Long> idList = query.setParameterList("resourceIds", currSet).list();
+            currSet = new HashSet<Long>(idList);
+            currSet.remove(result);
+            result.addAll(idList);
+        }
+
+        return result;
+    }
+
+    protected HashSet<Long> getAllGroupIds(Session session, Long resId) {
+        HashSet<Long> directGroupIds = getDirectGroupIds(session, resId);
+        HashSet<Long> result = getIndirectGroupIds(session, directGroupIds);
+        result.addAll(directGroupIds);
+        return result;
+    }
+
+}
--- src/main/java/it/infn/security/saml/datasource/hibernate/HibernateDataSource.java
+++ src/main/java/it/infn/security/saml/datasource/hibernate/HibernateDataSource.java
@@ -1,33 +1,20 @@
 package it.infn.security.saml.datasource.hibernate;
 
-import it.infn.security.saml.datasource.DataSource;
 import it.infn.security.saml.datasource.DataSourceException;
 import it.infn.security.saml.datasource.jpa.AttributeEntity;
 import it.infn.security.saml.datasource.jpa.AttributeEntityId;
-import it.infn.security.saml.datasource.jpa.ResourceEntity;
 import it.infn.security.saml.datasource.jpa.ResourceEntity.ResourceType;
 import it.infn.security.saml.datasource.jpa.UserEntity;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.hibernate.Query;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.opensaml.Configuration;
-import org.opensaml.saml2.core.Attribute;
-import org.opensaml.saml2.core.AttributeValue;
-import org.opensaml.saml2.core.impl.AttributeBuilder;
-import org.opensaml.xml.XMLObject;
-import org.opensaml.xml.XMLObjectBuilderFactory;
-import org.opensaml.xml.schema.XSString;
-import org.opensaml.xml.schema.impl.XSStringBuilder;
+import org.wso2.charon.core.attributes.Attribute;
 import org.wso2.charon.core.attributes.ComplexAttribute;
 import org.wso2.charon.core.attributes.MultiValuedAttribute;
 import org.wso2.charon.core.attributes.SimpleAttribute;
@@ -38,167 +25,11 @@ import org.wso2.charon.core.objects.Group;
 import org.wso2.charon.core.objects.User;
 
 public class HibernateDataSource
-    implements DataSource {
+    extends HibernateBaseDataSource {
 
     private static final Logger logger = Logger.getLogger(HibernateDataSource.class.getName());
 
-    /*
-     * Definitions for specific attribute structures
-     */
-    private final static String SPID_ATTR_NAME = "SPIDAttributes";
-
-    private final static String KEY_FIELD = "key";
-
-    private final static String CONTENT_FIELD = "content";
-
-    private final static String ATTR_DESCR_FIELD = "description";
-
-    private static SessionFactory sessionFactory;
-
     public HibernateDataSource() {
-    }
-
-    public void init()
-        throws DataSourceException {
-
-        try {
-
-            StandardServiceRegistryBuilder serviceRegistryBuilder = new StandardServiceRegistryBuilder();
-            serviceRegistryBuilder.applySettings(HibernateUtils.getHibernateConfig().getProperties());
-            sessionFactory = HibernateUtils.getHibernateConfig().buildSessionFactory(serviceRegistryBuilder.build());
-
-        } catch (Throwable th) {
-            logger.log(Level.SEVERE, "Cannot initialize database", th);
-            throw new DataSourceException("Cannot initialize database", th);
-        }
-
-    }
-
-    public List<Attribute> findAttributes(String id, List<Attribute> requiredAttrs)
-        throws DataSourceException {
-
-        ArrayList<Attribute> result = new ArrayList<Attribute>();
-
-        XMLObjectBuilderFactory builderFactory = Configuration.getBuilderFactory();
-        AttributeBuilder attributeBuilder = (AttributeBuilder) builderFactory
-                .getBuilder(Attribute.DEFAULT_ELEMENT_NAME);
-        XSStringBuilder attributeValueBuilder = (XSStringBuilder) builderFactory.getBuilder(XSString.TYPE_NAME);
-
-        Session session = sessionFactory.getCurrentSession();
-
-        try {
-
-            session.beginTransaction();
-
-            String qStr = "SELECT qUser.id FROM UserEntity as qUser WHERE qUser.userName = :uName";
-            Query query2 = session.createQuery(qStr).setString("uName", id);
-            Long userId = (Long) query2.uniqueResult();
-            if (userId == null) {
-                logger.info("Entity not found " + id);
-                /*
-                 * TODO empty list or error?
-                 */
-                // session.getTransaction().commit();
-                throw new DataSourceException("User not found");
-            }
-
-            HashSet<Long> allIds = getAllGroupIds(session, userId);
-            allIds.add(userId);
-
-            HashMap<String, Object> qArgs = new HashMap<String, Object>();
-            StringBuffer queryStr = new StringBuffer("SELECT qAttributes");
-            queryStr.append(" FROM ResourceEntity as qRes INNER JOIN qRes.attributes as qAttributes");
-
-            queryStr.append(" WHERE qRes.id IN (:resourceIds)");
-            qArgs.put("resourceIds", allIds);
-
-            if (requiredAttrs != null && requiredAttrs.size() > 0) {
-                queryStr.append(" AND (");
-                int keyNum = 0;
-                for (Attribute reqAttr : requiredAttrs) {
-                    String tmpName = reqAttr.getName();
-                    List<XMLObject> tmpValues = reqAttr.getAttributeValues();
-
-                    if (keyNum > 0) {
-                        queryStr.append(" OR");
-                    }
-                    String keyTag = "key_" + keyNum;
-                    keyNum++;
-
-                    if (tmpValues != null && tmpValues.size() > 0) {
-                        int cntNum = 0;
-                        for (XMLObject xObj : tmpValues) {
-
-                            if (cntNum > 0) {
-                                queryStr.append(" OR");
-                            }
-                            String refValue = xObj.getDOM().getTextContent().trim();
-
-                            queryStr.append(" (qAttributes.attributeId.key = :").append(keyTag);
-                            qArgs.put(keyTag, tmpName);
-
-                            String cntTag = "cnt_" + keyNum + "_" + cntNum;
-                            queryStr.append(" AND qAttributes.attributeId.content = :");
-                            queryStr.append(cntTag).append(")");
-                            qArgs.put(cntTag, refValue);
-                            cntNum++;
-                        }
-                    } else {
-                        queryStr.append(" qAttributes.attributeId.key = :").append(keyTag);
-                        qArgs.put(keyTag, tmpName);
-                    }
-                }
-
-                queryStr.append(")");
-            }
-
-            Query query = session.createQuery(queryStr.toString());
-            query.setProperties(qArgs);
-
-            @SuppressWarnings("unchecked")
-            List<AttributeEntity> filteredAttrs = query.list();
-
-            HashMap<String, Attribute> resultTable = new HashMap<String, Attribute>();
-            for (AttributeEntity attrEnt : filteredAttrs) {
-
-                String attrKey = attrEnt.getAttributeId().getKey();
-                Attribute attribute = null;
-                if (resultTable.containsKey(attrKey)) {
-                    attribute = resultTable.get(attrKey);
-                } else {
-                    attribute = attributeBuilder.buildObject();
-                    attribute.setName(attrKey);
-                    attribute.setNameFormat(Attribute.BASIC);
-                    resultTable.put(attrKey, attribute);
-                }
-
-                XSString attributeValue = attributeValueBuilder.buildObject(AttributeValue.DEFAULT_ELEMENT_NAME,
-                        XSString.TYPE_NAME);
-                attributeValue.setValue(attrEnt.getAttributeId().getContent());
-                attribute.getAttributeValues().add(attributeValue);
-
-            }
-
-            for (Attribute tmpAttr : resultTable.values()) {
-                result.add(tmpAttr);
-            }
-
-            session.getTransaction().commit();
-
-        } catch (Throwable th) {
-
-            session.getTransaction().rollback();
-
-            logger.log(Level.SEVERE, th.getMessage(), th);
-
-        }
-
-        return result;
-    }
-
-    public void close()
-        throws DataSourceException {
-
     }
 
     public User getUser(String userId)
@@ -234,7 +65,7 @@ public class HibernateDataSource
         return null;
     }
 
-    public List<User> listUsersByAttribute(org.wso2.charon.core.attributes.Attribute attribute) {
+    public List<User> listUsersByAttribute(Attribute attribute) {
         return null;
     }
 
@@ -256,7 +87,7 @@ public class HibernateDataSource
         return null;
     }
 
-    public User updateUser(List<org.wso2.charon.core.attributes.Attribute> updatedAttributes) {
+    public User updateUser(List<Attribute> updatedAttributes) {
         return null;
     }
 
@@ -286,9 +117,8 @@ public class HibernateDataSource
 
             Set<AttributeEntity> eUserAttrs = new HashSet<AttributeEntity>();
 
-            org.wso2.charon.core.attributes.Attribute extAttribute = user.getAttribute(SPID_ATTR_NAME);
-            for (org.wso2.charon.core.attributes.Attribute subAttr : ((MultiValuedAttribute) extAttribute)
-                    .getValuesAsSubAttributes()) {
+            Attribute extAttribute = user.getAttribute(SPID_ATTR_NAME);
+            for (Attribute subAttr : ((MultiValuedAttribute) extAttribute).getValuesAsSubAttributes()) {
                 ComplexAttribute cplxAttr = (ComplexAttribute) subAttr;
                 SimpleAttribute keyAttr = (SimpleAttribute) cplxAttr.getSubAttribute(KEY_FIELD);
                 SimpleAttribute cntAttr = (SimpleAttribute) cplxAttr.getSubAttribute(CONTENT_FIELD);
@@ -344,7 +174,7 @@ public class HibernateDataSource
         return null;
     }
 
-    public List<Group> listGroupsByAttribute(org.wso2.charon.core.attributes.Attribute attribute)
+    public List<Group> listGroupsByAttribute(Attribute attribute)
         throws CharonException {
         return null;
     }
@@ -378,7 +208,7 @@ public class HibernateDataSource
         return null;
     }
 
-    public Group updateGroup(List<org.wso2.charon.core.attributes.Attribute> attributes)
+    public Group updateGroup(List<Attribute> attributes)
         throws CharonException {
         return null;
     }
@@ -386,61 +216,6 @@ public class HibernateDataSource
     public void deleteGroup(String groupId)
         throws NotFoundException, CharonException {
 
-    }
-
-    private HashSet<ResourceEntity> getDirectGroups(Session session, ResourceEntity resource) {
-        StringBuffer queryStr = new StringBuffer("SELECT resource.groups");
-        queryStr.append(" FROM ResourceEntity as resource WHERE resource.id=?");
-        Query query = session.createQuery(queryStr.toString());
-        @SuppressWarnings("unchecked")
-        List<ResourceEntity> idList = query.setLong(0, resource.getId()).list();
-        return new HashSet<ResourceEntity>(idList);
-    }
-
-    private HashSet<ResourceEntity> getIndirectGroups(Session session, HashSet<ResourceEntity> resources) {
-        return null;
-    }
-
-    private HashSet<ResourceEntity> getAllGroups(Session session, ResourceEntity resource) {
-        HashSet<ResourceEntity> directGroups = getDirectGroups(session, resource);
-        HashSet<ResourceEntity> result = getIndirectGroups(session, directGroups);
-        result.addAll(directGroups);
-        return result;
-    }
-
-    private HashSet<Long> getDirectGroupIds(Session session, Long resId) {
-        StringBuffer queryStr = new StringBuffer("SELECT rGroups.id");
-        queryStr.append(" FROM ResourceEntity as resource INNER JOIN resource.groups as rGroups");
-        queryStr.append(" WHERE resource.id=?");
-        Query query = session.createQuery(queryStr.toString());
-        @SuppressWarnings("unchecked")
-        List<Long> idList = query.setLong(0, resId).list();
-        return new HashSet<Long>(idList);
-    }
-
-    private HashSet<Long> getIndirectGroupIds(Session session, HashSet<Long> resIds) {
-        HashSet<Long> result = new HashSet<Long>();
-        HashSet<Long> currSet = resIds;
-        while (currSet.size() > 0) {
-            StringBuffer queryStr = new StringBuffer("SELECT rGroups.id");
-            queryStr.append(" FROM ResourceEntity as resource INNER JOIN resource.groups as rGroups");
-            queryStr.append(" WHERE resource.id IN (:resourceIds)");
-            Query query = session.createQuery(queryStr.toString());
-            @SuppressWarnings("unchecked")
-            List<Long> idList = query.setParameterList("resourceIds", currSet).list();
-            currSet = new HashSet<Long>(idList);
-            currSet.remove(result);
-            result.addAll(idList);
-        }
-
-        return result;
-    }
-
-    private HashSet<Long> getAllGroupIds(Session session, Long resId) {
-        HashSet<Long> directGroupIds = getDirectGroupIds(session, resId);
-        HashSet<Long> result = getIndirectGroupIds(session, directGroupIds);
-        result.addAll(directGroupIds);
-        return result;
     }
 
     private User userFromEntity(Session session, UserEntity usrEnt)
