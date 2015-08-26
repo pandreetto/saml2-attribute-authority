b800ad877f40b8f884dca71d8bb9ae8ceb414b98
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2015-08-26 11:52:57 +0200

    Imported complete endpoint classes

--- src/main/java/it/infn/security/saml/utils/charon/GroupResourceEndpoint.java
+++ src/main/java/it/infn/security/saml/utils/charon/GroupResourceEndpoint.java
@@ -16,6 +16,7 @@ import org.wso2.charon.core.exceptions.CharonException;
 import org.wso2.charon.core.exceptions.InternalServerException;
 import org.wso2.charon.core.exceptions.NotFoundException;
 import org.wso2.charon.core.exceptions.ResourceNotFoundException;
+import org.wso2.charon.core.extensions.Storage;
 import org.wso2.charon.core.extensions.UserManager;
 import org.wso2.charon.core.objects.Group;
 import org.wso2.charon.core.objects.ListedResource;
@@ -24,12 +25,41 @@ import org.wso2.charon.core.protocol.SCIMResponse;
 import org.wso2.charon.core.protocol.endpoints.AbstractResourceEndpoint;
 import org.wso2.charon.core.schema.SCIMConstants;
 import org.wso2.charon.core.schema.SCIMResourceSchema;
+import org.wso2.charon.core.schema.SCIMSchemaDefinitions;
 import org.wso2.charon.core.schema.ServerSideValidator;
 
 public class GroupResourceEndpoint
-    extends org.wso2.charon.core.protocol.endpoints.GroupResourceEndpoint {
+    extends AbstractResourceEndpoint {
 
     private static Logger logger = Logger.getLogger(GroupResourceEndpoint.class.getName());
+
+    @Override
+    public SCIMResponse get(String id, String format, UserManager userManager) {
+
+        Encoder encoder = null;
+        try {
+            encoder = getEncoder(SCIMConstants.identifyFormat(format));
+
+            Group group = ((UserManager) userManager).getGroup(id);
+
+            if (group == null) {
+                String message = "Group not found in the user store.";
+                throw new ResourceNotFoundException(message);
+            }
+            ServerSideValidator.validateRetrievedSCIMObject(group, SCIMSchemaDefinitions.SCIM_GROUP_SCHEMA);
+            String encodedGroup = encoder.encodeSCIMObject(group);
+            Map<String, String> httpHeaders = new HashMap<String, String>();
+            httpHeaders.put(SCIMConstants.CONTENT_TYPE_HEADER, format);
+            return new SCIMResponse(ResponseCodeConstants.CODE_OK, encodedGroup, httpHeaders);
+
+        } catch (AbstractCharonException ex) {
+            if (ex instanceof CharonException && ex.getCode() == -1) {
+                ex.setCode(ResponseCodeConstants.CODE_INTERNAL_SERVER_ERROR);
+            }
+            logger.log(Level.FINE, ex.getMessage(), ex);
+            return AbstractResourceEndpoint.encodeSCIMException(encoder, ex);
+        }
+    }
 
     @Override
     public SCIMResponse create(String scimObjectString, String inputFormat, String outputFormat, UserManager userManager) {
@@ -64,16 +94,59 @@ public class GroupResourceEndpoint
 
             return new SCIMResponse(ResponseCodeConstants.CODE_CREATED, encodedGroup, httpHeaders);
 
-        } catch (CharonException ex) {
-            logger.log(Level.FINE, ex.getMessage(), ex);
-            if (ex.getCode() == -1) {
+        } catch (AbstractCharonException ex) {
+            if (ex instanceof CharonException && ex.getCode() == -1) {
                 ex.setCode(ResponseCodeConstants.CODE_INTERNAL_SERVER_ERROR);
             }
-            return AbstractResourceEndpoint.encodeSCIMException(encoder, ex);
-        } catch (AbstractCharonException ex) {
             logger.log(Level.FINE, ex.getMessage(), ex);
             return AbstractResourceEndpoint.encodeSCIMException(encoder, ex);
         }
+    }
+
+    @Override
+    public SCIMResponse delete(String id, Storage storage, String outputFormat) {
+        Encoder encoder = null;
+        try {
+            encoder = getEncoder(SCIMConstants.identifyFormat(outputFormat));
+            if (storage instanceof UserManager) {
+                ((UserManager) storage).deleteGroup(id);
+            } else {
+                String message = "Provided storage handler is not an implementation of UserManager";
+                throw new InternalServerException(message);
+            }
+            return new SCIMResponse(ResponseCodeConstants.CODE_OK, null, null);
+        } catch (AbstractCharonException ex) {
+            if (ex instanceof CharonException && ex.getCode() == -1) {
+                ex.setCode(ResponseCodeConstants.CODE_INTERNAL_SERVER_ERROR);
+            }
+            logger.log(Level.FINE, ex.getMessage(), ex);
+            return AbstractResourceEndpoint.encodeSCIMException(encoder, ex);
+        }
+    }
+
+    @Override
+    public SCIMResponse listByAttribute(String searchAttribute, UserManager userManager, String format) {
+        return null;
+    }
+
+    @Override
+    public SCIMResponse listByFilter(String filterString, UserManager userManager, String format) {
+        return null;
+    }
+
+    @Override
+    public SCIMResponse listBySort(String sortBy, String sortOrder, UserManager usermanager, String format) {
+        return null;
+    }
+
+    @Override
+    public SCIMResponse listWithPagination(int startIndex, int count, UserManager userManager, String format) {
+        return null;
+    }
+
+    @Override
+    public SCIMResponse list(UserManager userManager, String format) {
+        return null;
     }
 
     public SCIMResponse listByParams(String filterString, String sortBy, String sortOrder, int startIndex, int count,
@@ -97,10 +170,73 @@ public class GroupResourceEndpoint
             httpHeaders.put(SCIMConstants.CONTENT_TYPE_HEADER, format);
             return new SCIMResponse(ResponseCodeConstants.CODE_OK, encodedListedResource, httpHeaders);
 
-        } catch (AbstractCharonException chEx) {
-            logger.log(Level.SEVERE, chEx.getMessage(), chEx);
-            return AbstractResourceEndpoint.encodeSCIMException(encoder, chEx);
+        } catch (AbstractCharonException ex) {
+            if (ex instanceof CharonException && ex.getCode() == -1) {
+                ex.setCode(ResponseCodeConstants.CODE_INTERNAL_SERVER_ERROR);
+            }
+            logger.log(Level.FINE, ex.getMessage(), ex);
+            return AbstractResourceEndpoint.encodeSCIMException(encoder, ex);
         }
+    }
+
+    @Override
+    public SCIMResponse updateWithPUT(String existingId, String scimObjectString, String inputFormat,
+            String outputFormat, UserManager userManager) {
+        Encoder encoder = null;
+        Decoder decoder = null;
+
+        try {
+            encoder = getEncoder(SCIMConstants.identifyFormat(outputFormat));
+            decoder = getDecoder(SCIMConstants.identifyFormat(inputFormat));
+
+            Group group = (Group) decoder.decodeResource(scimObjectString, SCIMSchemaDefinitions.SCIM_GROUP_SCHEMA,
+                    new Group());
+            Group updatedGroup = null;
+            if (userManager != null) {
+                Group oldGroup = userManager.getGroup(existingId);
+                if (oldGroup != null) {
+                    Group validatedGroup = (Group) ServerSideValidator.validateUpdatedSCIMObject(oldGroup, group,
+                            SCIMSchemaDefinitions.SCIM_GROUP_SCHEMA);
+                    updatedGroup = userManager.updateGroup(oldGroup, validatedGroup);
+                } else {
+                    String message = "No group exists with the given id: " + existingId;
+                    throw new ResourceNotFoundException(message);
+                }
+
+            } else {
+                String message = "Provided User manager handler is null.";
+                throw new InternalServerException(message);
+            }
+
+            String encodedGroup;
+            Map<String, String> httpHeaders = new HashMap<String, String>();
+            if (updatedGroup != null) {
+
+                encodedGroup = encoder.encodeSCIMObject(updatedGroup);
+                httpHeaders.put(SCIMConstants.LOCATION_HEADER, getResourceEndpointURL(SCIMConstants.USER_ENDPOINT)
+                        + "/" + updatedGroup.getId());
+                httpHeaders.put(SCIMConstants.CONTENT_TYPE_HEADER, outputFormat);
+
+            } else {
+                String message = "Updated User resource is null..";
+                throw new InternalServerException(message);
+            }
+
+            return new SCIMResponse(ResponseCodeConstants.CODE_OK, encodedGroup, httpHeaders);
+
+        } catch (AbstractCharonException ex) {
+            if (ex instanceof CharonException && ex.getCode() == -1) {
+                ex.setCode(ResponseCodeConstants.CODE_INTERNAL_SERVER_ERROR);
+            }
+            logger.log(Level.FINE, ex.getMessage(), ex);
+            return AbstractResourceEndpoint.encodeSCIMException(encoder, ex);
+        }
+    }
+
+    @Override
+    public SCIMResponse updateWithPATCH(String existingId, String scimObjectString, String inputFormat,
+            String outputFormat, UserManager userManager) {
+        return null;
     }
 
     private ListedResource createListedResource(GroupSearchResult searchResult)
--- src/main/java/it/infn/security/saml/utils/charon/UserResourceEndpoint.java
+++ src/main/java/it/infn/security/saml/utils/charon/UserResourceEndpoint.java
@@ -9,21 +9,57 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.wso2.charon.core.attributes.Attribute;
+import org.wso2.charon.core.encoder.Decoder;
 import org.wso2.charon.core.encoder.Encoder;
 import org.wso2.charon.core.exceptions.AbstractCharonException;
 import org.wso2.charon.core.exceptions.CharonException;
+import org.wso2.charon.core.exceptions.InternalServerException;
 import org.wso2.charon.core.exceptions.ResourceNotFoundException;
+import org.wso2.charon.core.extensions.Storage;
+import org.wso2.charon.core.extensions.UserManager;
 import org.wso2.charon.core.objects.ListedResource;
 import org.wso2.charon.core.objects.User;
 import org.wso2.charon.core.protocol.ResponseCodeConstants;
 import org.wso2.charon.core.protocol.SCIMResponse;
 import org.wso2.charon.core.protocol.endpoints.AbstractResourceEndpoint;
 import org.wso2.charon.core.schema.SCIMConstants;
+import org.wso2.charon.core.schema.SCIMResourceSchema;
+import org.wso2.charon.core.schema.SCIMResourceSchemaManager;
+import org.wso2.charon.core.schema.ServerSideValidator;
+import org.wso2.charon.core.util.CopyUtil;
 
 public class UserResourceEndpoint
-    extends org.wso2.charon.core.protocol.endpoints.UserResourceEndpoint {
+    extends AbstractResourceEndpoint {
 
     private static Logger logger = Logger.getLogger(UserResourceEndpoint.class.getName());
+
+    @Override
+    public SCIMResponse get(String id, String format, UserManager userManager) {
+
+        Encoder encoder = null;
+        try {
+            encoder = getEncoder(SCIMConstants.identifyFormat(format));
+
+            User user = ((UserManager) userManager).getUser(id);
+            if (user == null) {
+                throw new ResourceNotFoundException("User not found in the user store.");
+            }
+
+            SCIMResourceSchema schema = SCIMResourceSchemaManager.getInstance().getUserResourceSchema();
+            ServerSideValidator.validateRetrievedSCIMObject(user, schema);
+            String encodedUser = encoder.encodeSCIMObject(user);
+            Map<String, String> httpHeaders = new HashMap<String, String>();
+            httpHeaders.put(SCIMConstants.CONTENT_TYPE_HEADER, format);
+            return new SCIMResponse(ResponseCodeConstants.CODE_OK, encodedUser, httpHeaders);
+
+        } catch (AbstractCharonException ex) {
+            if (ex instanceof CharonException && ex.getCode() == -1) {
+                ex.setCode(ResponseCodeConstants.CODE_INTERNAL_SERVER_ERROR);
+            }
+            logger.log(Level.FINE, ex.getMessage(), ex);
+            return AbstractResourceEndpoint.encodeSCIMException(encoder, ex);
+        }
+    }
 
     public SCIMResponse listByParams(String filterString, String sortBy, String sortOrder, int startIndex, int count,
             DataSource dataSource, String format) {
@@ -35,8 +71,7 @@ public class UserResourceEndpoint
 
             UserSearchResult searchResult = dataSource.listUsers(filterString, sortBy, sortOrder, startIndex, count);
             if (searchResult == null || searchResult.isEmpty()) {
-                String error = "Users not found in the user store for the filter: " + filterString;
-                throw new ResourceNotFoundException(error);
+                throw new ResourceNotFoundException("Users not found in the user store for the filter: " + filterString);
             }
 
             ListedResource listedResource = buildListedResource(searchResult);
@@ -45,17 +80,171 @@ public class UserResourceEndpoint
             httpHeaders.put(SCIMConstants.CONTENT_TYPE_HEADER, format);
             return new SCIMResponse(ResponseCodeConstants.CODE_OK, encodedListedResource, httpHeaders);
 
-        } catch (AbstractCharonException chEx) {
-            logger.log(Level.SEVERE, chEx.getMessage(), chEx);
-            return AbstractResourceEndpoint.encodeSCIMException(encoder, chEx);
+        } catch (AbstractCharonException ex) {
+            if (ex instanceof CharonException && ex.getCode() == -1) {
+                ex.setCode(ResponseCodeConstants.CODE_INTERNAL_SERVER_ERROR);
+            }
+            logger.log(Level.FINE, ex.getMessage(), ex);
+            return AbstractResourceEndpoint.encodeSCIMException(encoder, ex);
         }
+    }
+
+    @Override
+    public SCIMResponse listByAttribute(String searchAttribute, UserManager userManager, String format) {
+        return null;
+    }
+
+    @Override
+    public SCIMResponse listByFilter(String filterString, UserManager userManager, String format) {
+        return null;
+    }
+
+    @Override
+    public SCIMResponse listBySort(String sortBy, String sortOrder, UserManager usermanager, String format) {
+        return null;
+    }
+
+    @Override
+    public SCIMResponse listWithPagination(int startIndex, int count, UserManager userManager, String format) {
+        return null;
+    }
+
+    @Override
+    public SCIMResponse list(UserManager userManager, String format) {
+        return null;
+    }
+
+    public SCIMResponse create(String scimObjectString, String inputFormat, String outputFormat,
+            UserManager userManager, boolean isBulkUserAdd) {
+
+        Encoder encoder = null;
+
+        try {
+            encoder = getEncoder(SCIMConstants.identifyFormat(outputFormat));
+            Decoder decoder = getDecoder(SCIMConstants.identifyFormat(inputFormat));
+
+            SCIMResourceSchema schema = SCIMResourceSchemaManager.getInstance().getUserResourceSchema();
+
+            User user = (User) decoder.decodeResource(scimObjectString, schema, new User());
+
+            ServerSideValidator.validateCreatedSCIMObject(user, schema);
+            User createdUser = userManager.createUser(user, isBulkUserAdd);
+
+            String encodedUser;
+            Map<String, String> httpHeaders = new HashMap<String, String>();
+            if (createdUser != null) {
+                User copiedUser = (User) CopyUtil.deepCopy(createdUser);
+
+                ServerSideValidator.removePasswordOnReturn(copiedUser);
+                encodedUser = encoder.encodeSCIMObject(copiedUser);
+                httpHeaders.put(SCIMConstants.LOCATION_HEADER, getResourceEndpointURL(SCIMConstants.USER_ENDPOINT)
+                        + "/" + createdUser.getId());
+                httpHeaders.put(SCIMConstants.CONTENT_TYPE_HEADER, outputFormat);
+
+            } else {
+                throw new InternalServerException("Newly created User resource is null");
+            }
+
+            return new SCIMResponse(ResponseCodeConstants.CODE_CREATED, encodedUser, httpHeaders);
+
+        } catch (AbstractCharonException ex) {
+            if (ex instanceof CharonException && ex.getCode() == -1) {
+                ex.setCode(ResponseCodeConstants.CODE_INTERNAL_SERVER_ERROR);
+            }
+            logger.log(Level.FINE, ex.getMessage(), ex);
+            return AbstractResourceEndpoint.encodeSCIMException(encoder, ex);
+        }
+
+    }
+
+    @Override
+    public SCIMResponse create(String scimObjStr, String inFormat, String outFormat, UserManager userManager) {
+
+        return create(scimObjStr, inFormat, outFormat, userManager, false);
+    }
+
+    @Override
+    public SCIMResponse delete(String id, Storage storage, String outputFormat) {
+        Encoder encoder = null;
+        try {
+            encoder = getEncoder(SCIMConstants.identifyFormat(outputFormat));
+            if (storage instanceof UserManager) {
+                ((UserManager) storage).deleteUser(id);
+            } else {
+                throw new InternalServerException("Storage handler is not an implementation of UserManager");
+            }
+            return new SCIMResponse(ResponseCodeConstants.CODE_OK, null, null);
+        } catch (AbstractCharonException ex) {
+            if (ex instanceof CharonException && ex.getCode() == -1) {
+                ex.setCode(ResponseCodeConstants.CODE_INTERNAL_SERVER_ERROR);
+            }
+            logger.log(Level.FINE, ex.getMessage(), ex);
+            return AbstractResourceEndpoint.encodeSCIMException(encoder, ex);
+        }
+    }
+
+    @Override
+    public SCIMResponse updateWithPUT(String existingId, String scimObjectString, String inputFormat,
+            String outputFormat, UserManager userManager) {
+
+        Encoder encoder = null;
+        Decoder decoder = null;
+
+        try {
+            encoder = getEncoder(SCIMConstants.identifyFormat(outputFormat));
+            decoder = getDecoder(SCIMConstants.identifyFormat(inputFormat));
+
+            SCIMResourceSchema schema = SCIMResourceSchemaManager.getInstance().getUserResourceSchema();
+
+            User user = (User) decoder.decodeResource(scimObjectString, schema, new User());
+            User updatedUser = null;
+            if (userManager != null) {
+                User oldUser = userManager.getUser(existingId);
+                if (oldUser != null) {
+                    User validatedUser = (User) ServerSideValidator.validateUpdatedSCIMObject(oldUser, user, schema);
+                    updatedUser = userManager.updateUser(validatedUser);
+
+                } else {
+                    throw new ResourceNotFoundException("No user exists with the given id: " + existingId);
+                }
+
+            } else {
+                throw new InternalServerException("Provided user manager handler is null.");
+            }
+
+            String encodedUser;
+            Map<String, String> httpHeaders = new HashMap<String, String>();
+            if (updatedUser != null) {
+                User copiedUser = (User) CopyUtil.deepCopy(updatedUser);
+                ServerSideValidator.removePasswordOnReturn(copiedUser);
+                encodedUser = encoder.encodeSCIMObject(copiedUser);
+                httpHeaders.put(SCIMConstants.LOCATION_HEADER, getResourceEndpointURL(SCIMConstants.USER_ENDPOINT)
+                        + "/" + updatedUser.getId());
+                httpHeaders.put(SCIMConstants.CONTENT_TYPE_HEADER, outputFormat);
+
+            } else {
+                throw new InternalServerException("Updated User resource is null");
+            }
+
+            return new SCIMResponse(ResponseCodeConstants.CODE_OK, encodedUser, httpHeaders);
+
+        } catch (AbstractCharonException ex) {
+            if (ex instanceof CharonException && ex.getCode() == -1) {
+                ex.setCode(ResponseCodeConstants.CODE_INTERNAL_SERVER_ERROR);
+            }
+            logger.log(Level.FINE, ex.getMessage(), ex);
+            return AbstractResourceEndpoint.encodeSCIMException(encoder, ex);
+        }
+    }
+
+    @Override
+    public SCIMResponse updateWithPATCH(String existingId, String scimObjectString, String inputFormat,
+            String outputFormat, UserManager userManager) {
+        return null;
     }
 
     private ListedResource buildListedResource(UserSearchResult searchResult)
         throws CharonException {
-        /*
-         * copy from org.wso2.charon.core.protocol.endpoints.UserResourceEndpoint
-         */
         ListedResource listedResource = new ListedResource();
         listedResource.setTotalResults(searchResult.getTotalResults());
         for (User user : searchResult.getUserList()) {
