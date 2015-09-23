1a606e73684bad295c579a21531a7c2b6a4a3a9b
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2015-09-23 12:08:15 +0200

    Imported class from charon-utils

---
+++ src/main/java/it/infn/security/saml/utils/charon/JAXRSResponseBuilder.java
@@ -0,0 +1,32 @@
+package it.infn.security.saml.utils.charon;
+
+import org.wso2.charon.core.protocol.SCIMResponse;
+
+import javax.ws.rs.core.Response;
+import java.util.Map;
+
+public class JAXRSResponseBuilder {
+
+    public static Response buildResponse(int code, Map<String, String> httpHeaders, String message) {
+
+        Response.ResponseBuilder responseBuilder = Response.status(code);
+
+        if (httpHeaders != null && httpHeaders.size() != 0) {
+            for (Map.Entry<String, String> entry : httpHeaders.entrySet()) {
+                responseBuilder.header(entry.getKey(), entry.getValue());
+            }
+        }
+
+        if (message != null) {
+            responseBuilder.entity(message);
+        }
+
+        return responseBuilder.build();
+    }
+
+    public static Response buildResponse(SCIMResponse scimResponse) {
+        return buildResponse(scimResponse.getResponseCode(), scimResponse.getHeaderParameterMap(),
+                scimResponse.getResponseMessage());
+    }
+
+}
