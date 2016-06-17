fcd73fec2ebd5b5b3f91fa2eedf9c246a55b99ee
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2016-06-17 16:25:11 +0200

    Further steps for the decoder

--- src/main/java/it/infn/security/scim/core/SCIM2Decoder.java
+++ src/main/java/it/infn/security/scim/core/SCIM2Decoder.java
@@ -3,6 +3,10 @@ package it.infn.security.scim.core;
 import it.infn.security.saml.datasource.DataSourceException;
 
 import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import javax.json.Json;
 import javax.json.stream.JsonParser;
@@ -18,17 +22,27 @@ public class SCIM2Decoder {
         return jParser.getString();
     }
 
-    private static boolean checkResAttribute(SCIM2Resource resource, String kName, String value)
+    private static void checkSchemas(JsonParser jParser, Set<String> schemas) {
+        for (JsonParser.Event evn = jParser.next(); evn != JsonParser.Event.END_ARRAY; evn = jParser.next()) {
+            if (evn == JsonParser.Event.VALUE_STRING) {
+                schemas.add(jParser.getString());
+            } else {
+                throw new JsonParsingException("Bad schema definition", jParser.getLocation());
+            }
+        }
+    }
+
+    private static void checkResAttribute(JsonParser jParser, SCIM2Resource resource, String kName, String value)
         throws DataSourceException {
-        if ("id".equals(kName)) {
+
+        if (SCIMCoreConstants.ID.equals(kName)) {
             resource.setResourceId(value);
-            return true;
-        }
-        if ("externalId".equals(kName)) {
+        } else if (SCIMCoreConstants.EXTERNAL_ID.equals(kName)) {
             resource.setResourceExtId(value);
-            return true;
+        } else {
+            throw new JsonParsingException("Attribute not recognized " + kName, jParser.getLocation());
         }
-        return false;
+
     }
 
     private static void checkMeta(SCIM2Resource resource, JsonParser jParser)
@@ -43,18 +57,136 @@ public class SCIM2Decoder {
         }
     }
 
-    private static boolean checkUserAttribute(SCIM2User user, String kName, String value)
+    private static void checkAttribute(JsonParser jParser, SCIM2User user, String kName, String value)
         throws DataSourceException {
-        if ("userName".equals(kName)) {
+
+        if (SCIMCoreConstants.USER_NAME.equals(kName)) {
             user.setName(value);
-            return true;
+        } else if (SCIMCoreConstants.DISPLAY_NAME.equals(kName)) {
+
+        } else if (SCIMCoreConstants.PROFILE_URL.equals(kName)) {
+
+        } else if (SCIMCoreConstants.TITLE.equals(kName)) {
+
+        } else if (SCIMCoreConstants.USER_TYPE.equals(kName)) {
+
+        } else if (SCIMCoreConstants.PREFERRED_LANGUAGE.equals(kName)) {
+
+        } else if (SCIMCoreConstants.LOCALE.equals(kName)) {
+
+        } else if (SCIMCoreConstants.TIME_ZONE.equals(kName)) {
+
+        } else if (SCIMCoreConstants.PASSWORD.equals(kName)) {
+
+        } else {
+
+            checkResAttribute(jParser, user, kName, value);
+
         }
-        return false;
+
     }
 
-    private static boolean checkUserAttribute(SCIM2User user, String kName, int value)
+    private static void checkAttribute(JsonParser jParser, SCIM2Group group, String kName, String value)
         throws DataSourceException {
-        return false;
+        if (SCIMCoreConstants.DISPLAY_NAME.equals(kName)) {
+        } else {
+
+            checkResAttribute(jParser, group, kName, value);
+
+        }
+
+    }
+
+    private static void checkSubAttribute(JsonParser jParser, List<String[]> attrList) {
+
+        String[] aTuple = new String[] { null, null };
+
+        for (JsonParser.Event evn = jParser.next(); evn != JsonParser.Event.END_OBJECT; evn = jParser.next()) {
+            String kName = null;
+
+            if (evn == JsonParser.Event.KEY_NAME) {
+                kName = jParser.getString();
+            } else if (evn == JsonParser.Event.VALUE_STRING) {
+                if (SCIMCoreConstants.VALUE.equals(kName)) {
+                    aTuple[0] = jParser.getString();
+                } else if (SCIMCoreConstants.PRIMARY.equals(kName)) {
+                    // TODO ignored
+                } else if (SCIMCoreConstants.TYPE.equals(kName)) {
+                    aTuple[1] = jParser.getString();
+                } else if (SCIMCoreConstants.DISPLAY.equals(kName)) {
+                    // TODO ignored
+                } else {
+                    throw new JsonParsingException("Wrong subattribute format", jParser.getLocation());
+                }
+            } else {
+                throw new JsonParsingException("Wrong subattribute format", jParser.getLocation());
+            }
+        }
+
+        attrList.add(aTuple);
+    }
+
+    private static void checkStdMultiValue(JsonParser jParser, SCIM2User user, String kName)
+        throws DataSourceException {
+
+        List<String[]> attrList = new ArrayList<String[]>();
+
+        for (JsonParser.Event evn = jParser.next(); evn != JsonParser.Event.END_ARRAY; evn = jParser.next()) {
+
+            if (evn == JsonParser.Event.VALUE_STRING) {
+                // primitive value
+                attrList.add(new String[] { jParser.getString(), "undefined" });
+            } else if (evn == JsonParser.Event.START_OBJECT) {
+                // complex value
+                checkSubAttribute(jParser, attrList);
+            } else {
+                throw new JsonParsingException("Wrong json format", jParser.getLocation());
+            }
+        }
+
+        if (SCIMCoreConstants.EMAILS.equals(kName)) {
+            for (String[] aTuple : attrList) {
+                user.addUserEmail(aTuple[0], aTuple[1]);
+            }
+        } else if (SCIMCoreConstants.PHONE_NUMBERS.equals(kName)) {
+            for (String[] aTuple : attrList) {
+                user.addUserPhone(aTuple[0], aTuple[1]);
+            }
+        } else if (SCIMCoreConstants.IMS.equals(kName)) {
+            for (String[] aTuple : attrList) {
+                user.addUserIM(aTuple[0], aTuple[1]);
+            }
+        } else if (SCIMCoreConstants.PHOTOS.equals(kName)) {
+            for (String[] aTuple : attrList) {
+                user.addUserPhoto(aTuple[0], aTuple[1]);
+            }
+        } else if (SCIMCoreConstants.ENTITLEMENTS.equals(kName)) {
+            for (String[] aTuple : attrList) {
+                user.addUserEntitle(aTuple[0], aTuple[1]);
+            }
+        } else if (SCIMCoreConstants.ROLES.equals(kName)) {
+            for (String[] aTuple : attrList) {
+                user.addUserRole(aTuple[0], aTuple[1]);
+            }
+        } else if (SCIMCoreConstants.X509CERTIFICATES.equals(kName)) {
+            for (String[] aTuple : attrList) {
+                user.addUserCertificate(aTuple[0], aTuple[1]);
+            }
+        } else {
+            throw new JsonParsingException("Attribute not recognized " + kName, jParser.getLocation());
+        }
+    }
+
+    private static void checkBegin(JsonParser jParser) {
+        if (!jParser.hasNext() || jParser.next() != JsonParser.Event.START_OBJECT) {
+            throw new JsonParsingException("Wrong json format", jParser.getLocation());
+        }
+    }
+
+    private static void checkEnd(JsonParser jParser) {
+        if (!jParser.hasNext() || jParser.next() != JsonParser.Event.END_OBJECT) {
+            throw new JsonParsingException("Wrong json format", jParser.getLocation());
+        }
     }
 
     public static SCIM2User decodeUser(String inStr)
@@ -62,11 +194,10 @@ public class SCIM2Decoder {
 
         JsonParser jParser = Json.createParser(new StringReader(inStr));
         SCIM2User result = new SCIM2User();
+        Set<String> schemas = new HashSet<String>();
 
         try {
-            if (!jParser.hasNext() || jParser.next() != JsonParser.Event.START_OBJECT) {
-                throw new JsonParsingException("Wrong json format", jParser.getLocation());
-            }
+            checkBegin(jParser);
 
             String keyName = null;
 
@@ -79,35 +210,38 @@ public class SCIM2Decoder {
 
                 } else if (event == JsonParser.Event.VALUE_STRING) {
 
-                    String strValue = jParser.getString();
-                    if (checkResAttribute(result, keyName, strValue) || checkUserAttribute(result, keyName, strValue)) {
-                        keyName = null;
-                    } else {
-                        throw new RuntimeException("Wrong field " + keyName + " : " + strValue);
-                    }
-
-                } else if (event == JsonParser.Event.VALUE_NUMBER) {
-
-                    int intValue = jParser.getInt();
-                    if (checkUserAttribute(result, keyName, intValue)) {
-                        keyName = null;
-                    } else {
-                        throw new RuntimeException("Wrong field " + keyName + " : " + intValue);
-                    }
+                    checkAttribute(jParser, result, keyName, jParser.getString());
+                    keyName = null;
 
                 } else if (event == JsonParser.Event.START_OBJECT) {
 
-                    if ("meta".equals(keyName)) {
+                    if (SCIMCoreConstants.META.equals(keyName)) {
                         checkMeta(result, jParser);
+                    } else if (SCIMCoreConstants.NAME.equals(keyName)) {
+
+                    } else {
+                        throw new JsonParsingException("Attribute not recognized " + keyName, jParser.getLocation());
                     }
 
                 } else if (event == JsonParser.Event.START_ARRAY) {
 
+                    if (SCIMCoreConstants.SCHEMAS.equals(keyName)) {
+                        checkSchemas(jParser, schemas);
+                    } else if (SCIMCoreConstants.ADDRESSES.equals(keyName)) {
+
+                    } else if (SCIMCoreConstants.GROUPS.equals(keyName)) {
+
+                    } else {
+                        checkStdMultiValue(jParser, result, keyName);
+                    }
+
                 }
             }
 
-            if (!jParser.hasNext() || jParser.next() != JsonParser.Event.START_OBJECT) {
-                throw new JsonParsingException("Wrong json format", jParser.getLocation());
+            checkEnd(jParser);
+
+            if (!schemas.contains(SCIMCoreConstants.SCIM2_USER_SCHEMA)) {
+                throw new DataSourceException("User schema not defined");
             }
 
         } finally {
@@ -116,4 +250,62 @@ public class SCIM2Decoder {
 
         return result;
     }
+
+    public static SCIM2Group decodeGroup(String inStr)
+        throws DataSourceException {
+
+        JsonParser jParser = Json.createParser(new StringReader(inStr));
+        SCIM2Group result = new SCIM2Group();
+        Set<String> schemas = new HashSet<String>();
+
+        try {
+            checkBegin(jParser);
+
+            String keyName = null;
+
+            while (jParser.hasNext()) {
+                JsonParser.Event event = jParser.next();
+
+                if (event == JsonParser.Event.KEY_NAME) {
+
+                    keyName = getKeyName(jParser);
+
+                } else if (event == JsonParser.Event.VALUE_STRING) {
+
+                    checkAttribute(jParser, result, keyName, jParser.getString());
+
+                } else if (event == JsonParser.Event.START_OBJECT) {
+
+                    if (SCIMCoreConstants.META.equals(keyName)) {
+                        checkMeta(result, jParser);
+                    } else {
+                        throw new JsonParsingException("Attribute not recognized " + keyName, jParser.getLocation());
+                    }
+
+                } else if (event == JsonParser.Event.START_ARRAY) {
+
+                    if (SCIMCoreConstants.SCHEMAS.equals(keyName)) {
+                        checkSchemas(jParser, schemas);
+                    } else if (SCIMCoreConstants.MEMBERS.equals(keyName)) {
+
+                    } else {
+                        throw new JsonParsingException("Attribute not recognized " + keyName, jParser.getLocation());
+                    }
+
+                }
+            }
+
+            checkEnd(jParser);
+
+            if (!schemas.contains(SCIMCoreConstants.SCIM2_GROUP_SCHEMA)) {
+                throw new DataSourceException("Group schema not defined");
+            }
+
+        } finally {
+            jParser.close();
+        }
+
+        return result;
+    }
+
 }
--- src/main/java/it/infn/security/scim/core/SCIMCoreConstants.java
+++ src/main/java/it/infn/security/scim/core/SCIMCoreConstants.java
@@ -2,6 +2,20 @@ package it.infn.security.scim.core;
 
 public class SCIMCoreConstants {
 
+    public static final String SCIM2_USER_SCHEMA = "urn:ietf:params:scim:schemas:core:2.0:User";
+
+    public static final String SCIM2_GROUP_SCHEMA = "urn:ietf:params:scim:schemas:core:2.0:Group";
+
+    public static final String ID = "id";
+
+    public static final String EXTERNAL_ID = "externalId";
+
+    public static final String META = "meta";
+
+    public static final String NAME = "name";
+
+    public static final String USER_NAME = "userName";
+
     public static final String GIVEN_NAME = "givenName";
 
     public static final String FAMILY_NAME = "familyName";
@@ -32,16 +46,46 @@ public class SCIMCoreConstants {
 
     public static final String EMAIL = "email";
 
+    public static final String EMAILS = "emails";
+
     public static final String PHONE_NUMBER = "phoneNumber";
+
+    public static final String PHONE_NUMBERS = "phoneNumbers";
 
     public static final String IM = "im";
 
+    public static final String IMS = "ims";
+
     public static final String PHOTO = "photo";
+
+    public static final String PHOTOS = "photos";
 
     public static final String ROLE = "role";
 
+    public static final String ROLES = "roles";
+
     public static final String ENTITLEMENT = "entitlement";
 
+    public static final String ENTITLEMENTS = "entitlements";
+
     public static final String X509CERTIFICATE = "x509Certificate";
+
+    public static final String X509CERTIFICATES = "x509Certificates";
+
+    public static final String ADDRESSES = "addresses";
+
+    public static final String GROUPS = "groups";
+
+    public static final String MEMBERS = "members";
+
+    public static final String SCHEMAS = "schemas";
+
+    public static final String VALUE = "value";
+
+    public static final String TYPE = "type";
+
+    public static final String PRIMARY = "primary";
+
+    public static final String DISPLAY = "display";
 
 }
